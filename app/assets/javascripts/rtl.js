c46235c60b3e4b5e976b0828ba997f4fd7932fe9
Emil Sedgh <emilsedgh@kde.org>
2017-02-19 03:27:54 +0100

    direct

--- app/assets/javascripts/rtl.js
+++ app/assets/javascripts/rtl.js
@@ -4,11 +4,15 @@ TextDirection.RTL = 'rtl'
 TextDirection.LTR = 'ltr'
 
 
-TextDirection.getDirection = function(text) {
-    var rtlChar = /[\u0590-\u083F]|[\u08A0-\u08FF]|[\uFB1D-\uFDFF]|[\uFE70-\uFEFF]/mg;
+TextDirection.detect = function(text) {
+  var rtlChar = /[\u0590-\u083F]|[\u08A0-\u08FF]|[\uFB1D-\uFDFF]|[\uFE70-\uFEFF]/mg
 
-    if (text.match(text))
-      return TextDirection.RTL
+  if (text.match(text))
+    return TextDirection.RTL
 
-    return TextDirection.LTR
+  return TextDirection.LTR
+}
+
+TextDirection.direct = function(el) {
+  el.style.direction = TextDirection.detect(el.innerText)
 }
