57d9a1cac6c293b23e752f1b556b0c10a5ab0862
Emil Sedgh <emilsedgh@kde.org>
2017-02-19 03:24:11 +0100

    Define "constants"

--- app/assets/javascripts/rtl.js
+++ app/assets/javascripts/rtl.js
@@ -1,5 +1,9 @@
 TextDirection = {}
 
+TextDirection.RTL = 'rtl'
+TextDirection.LTR = 'ltr'
+
+
 TextDirection.getDirection = function(text) {
     var rtlChar = /[\u0590-\u083F]|[\u08A0-\u08FF]|[\uFB1D-\uFDFF]|[\uFE70-\uFEFF]/mg;
 
